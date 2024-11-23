12e0af61322d4e8ba85f398e57423f7a874c3549
jivan7617 <jivan7617@gmail.com>
2024-11-23 18:34:24 -0500

    primer commit1

---
+++ proyectoUdeA/src/main/java/com/UdeA/proyectoUdeA/ProyectoUdeAApplication.java
@@ -0,0 +1,13 @@
+package com.UdeA.proyectoUdeA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoUdeAApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoUdeAApplication.class, args);
+	}
+
+}
---
+++ proyectoUdeA/src/test/java/com/UdeA/proyectoUdeA/ProyectoUdeAApplicationTests.java
@@ -0,0 +1,13 @@
+package com.UdeA.proyectoUdeA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoUdeAApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
