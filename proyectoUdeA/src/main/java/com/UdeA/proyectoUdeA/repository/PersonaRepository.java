164254dd957630374a6d0a3030e4da8bf2c599f6
jivan7617 <jivan7617@gmail.com>
2024-11-27 14:55:44 -0500

    modificando repositorio usuario

---
+++ proyectoUdeA/src/main/java/com/UdeA/proyectoUdeA/repository/PersonaRepository.java
@@ -0,0 +1,8 @@
+package com.UdeA.proyectoUdeA.repository;
+
+import com.UdeA.proyectoUdeA.modelo.Persona;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PersonaRepository extends JpaRepository <Persona, String>{
+
+}
