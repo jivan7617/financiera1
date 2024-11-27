ad5fa6a3708ad813ba2a1392e64d70a359eb97ee
jivan7617 <jivan7617@gmail.com>
2024-11-27 14:57:23 -0500

    creando el controlador persona y usuario

---
+++ proyectoUdeA/src/main/java/com/UdeA/proyectoUdeA/Controler/UsuarioControler.java
@@ -0,0 +1,24 @@
+package com.UdeA.proyectoUdeA.Controler;
+
+import com.UdeA.proyectoUdeA.Service.UsuarioService;
+import com.UdeA.proyectoUdeA.modelo.Usuario;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UsuarioControler {
+    private final UsuarioService usuarioService;
+
+    public UsuarioControler(UsuarioService usuarioService) {
+        this.usuarioService = usuarioService;
+    }
+    @PostMapping("/insertarU")
+    public Usuario insertarUsuario(@RequestBody Usuario usuario) {
+        try{
+            return usuarioService.insertarUsuario(usuario);
+        } catch (RuntimeException error) {
+            throw new RuntimeException( "error al insertar la persona", error);
+        }
+    }
+}
