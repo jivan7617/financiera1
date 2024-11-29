d8280155f48f5119b4ac4148ba5edf825e9e44fc
jivan7617 <jivan7617@gmail.com>
2024-11-29 16:48:37 -0500

    agrgando creditoService

---
+++ proyectoUdeA/src/main/java/com/UdeA/proyectoUdeA/Service/CreditoService.java
@@ -0,0 +1,30 @@
+package com.UdeA.proyectoUdeA.Service;
+
+import com.UdeA.proyectoUdeA.modelo.Credito;
+import com.UdeA.proyectoUdeA.repository.CreditoRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+
+public class CreditoService {
+    private CreditoRepository creditoRepository;
+
+    public CreditoService(CreditoRepository creditoRepository) {
+        this.creditoRepository = creditoRepository;
+
+    }
+
+    public Credito getCredito(Credito credito) {
+        try {
+            return creditoRepository.save(credito);
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new RuntimeException("error al insertar los datos");
+        }
+    }
+    public List<Credito> consultarCredito(){
+        return creditoRepository.findAll();
+    }
+}
