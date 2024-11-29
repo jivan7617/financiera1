919e45e2abf69b0df53df1ff7f0be2d08d308334
jivan7617 <jivan7617@gmail.com>
2024-11-29 17:42:48 -0500

    creando el controlador de tipo de credito

---
+++ proyectoUdeA/src/main/java/com/UdeA/proyectoUdeA/Controler/TipoDeCreditoControler.java
@@ -0,0 +1,24 @@
+package com.UdeA.proyectoUdeA.Controler;
+
+import com.UdeA.proyectoUdeA.Service.TipoDeCreditoService;
+import com.UdeA.proyectoUdeA.modelo.TipoDeCredito;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TipoDeCreditoControler {
+    private final TipoDeCreditoService tipoDeCreditoService;
+
+    public TipoDeCreditoControler(TipoDeCreditoService tipoDeCreditoService) {
+        this.tipoDeCreditoService = tipoDeCreditoService;
+    }
+    @PostMapping("/insertar tipo credito")
+    public TipoDeCredito insertarTipoDeCredito(@RequestBody TipoDeCredito tipoDeCredito) {
+        try{
+            return tipoDeCreditoService.insertarTipoDeCredito(tipoDeCredito);
+        } catch (RuntimeException error) {
+            throw new RuntimeException( "error al insertar la tipo de credito", error);
+        }
+    }
+}
