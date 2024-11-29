7721e1feb9f0fd202638a9cff897f4e45b59bcee
jivan7617 <jivan7617@gmail.com>
2024-11-29 16:57:36 -0500

    agregando el decorador a requisito credito Service

--- proyectoUdeA/src/main/java/com/UdeA/proyectoUdeA/Service/RequisitoCreditoService.java
+++ proyectoUdeA/src/main/java/com/UdeA/proyectoUdeA/Service/RequisitoCreditoService.java
@@ -2,9 +2,10 @@ package com.UdeA.proyectoUdeA.Service;
 
 import com.UdeA.proyectoUdeA.modelo.RequisitosCredito;
 import com.UdeA.proyectoUdeA.repository.RequisitosCreditoRepository;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
-
+@Service
 public class RequisitoCreditoService {
     private RequisitosCreditoRepository requisitoCRepository;
 
