a418c38d48d0e2f848f62775af80fcd44f94381c
jivan7617 <jivan7617@gmail.com>
2024-12-07 18:48:43 -0500

    add service

---
+++ proyectoUdeA/src/main/java/com/UdeA/proyectoUdeA/servicce/CreditoService.java
@@ -0,0 +1,32 @@
+package com.UdeA.proyectoUdeA.servicce;
+
+import com.UdeA.proyectoUdeA.modelo.Credito;
+import com.UdeA.proyectoUdeA.repository.CreditoRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+
+public class CreditoService {
+    private CreditoRepository creditoRepository;
+
+    public CreditoService(CreditoRepository creditoRepository) {
+        this.creditoRepository = creditoRepository;
+
+    }
+
+
+    public Credito insertarCredito(Credito credito) {
+        try {
+            return creditoRepository.save(credito);
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new RuntimeException("error al insertar los datos");
+        }
+    }
+    public List<Credito> consultarCredito(){
+        return creditoRepository.findAll();
+    }
+
+}
---
+++ proyectoUdeA/src/main/java/com/UdeA/proyectoUdeA/servicce/PersonaService.java
@@ -0,0 +1,31 @@
+package com.UdeA.proyectoUdeA.servicce;
+
+import com.UdeA.proyectoUdeA.modelo.Persona;
+import com.UdeA.proyectoUdeA.repository.PersonaRepository;
+import org.springframework.dao.DataAccessException;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class PersonaService {
+    private PersonaRepository personaRepository;
+
+    public PersonaService(PersonaRepository personaRepository) {
+        this.personaRepository = personaRepository;
+    }
+
+    public Persona insertarPersona(Persona persona) {
+        try{
+            return personaRepository.save(persona);
+        }catch (DataAccessException error){
+            error.printStackTrace();
+            throw new RuntimeException("Error al insertar persona");
+        }
+    }
+
+    public List<Persona> consultarPersona(){
+        return personaRepository.findAll();
+    }
+
+}
