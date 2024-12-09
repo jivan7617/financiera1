97d17bd34ed8d4fb52e1abca2a71f1bcd4a57c73
jivan7617 <jivan7617@gmail.com>
2024-12-08 19:16:07 -0500

    change create to update

--- proyectoUdeA/src/main/java/com/UdeA/proyectoUdeA/controler/PersonControler.java
+++ proyectoUdeA/src/main/java/com/UdeA/proyectoUdeA/controler/PersonControler.java
@@ -1,11 +1,11 @@
 package com.UdeA.proyectoUdeA.controler;
 
+import com.UdeA.proyectoUdeA.modelo.Credito;
 import com.UdeA.proyectoUdeA.modelo.Persona;
 import com.UdeA.proyectoUdeA.servicce.PersonaService;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @CrossOrigin(origins = "http://localhost:5173")
@@ -16,11 +16,21 @@ public class PersonControler {
         this.personaService = personaService;
     }
     @PostMapping("/insertarP")
-    public Persona insertarDatos(@RequestBody Persona persona){
+    public Persona insertarP(@RequestBody Persona persona){
         try{
             return personaService.insertarPersona(persona);
         } catch (RuntimeException e) {
             throw new RuntimeException( "error al insertar la persona", e);
         }
     }
+
+    @GetMapping("/consultarP")
+    public List<Persona> consultarPersona() {
+        try {
+            return personaService.consultarPersona();
+        } catch (RuntimeException e) {
+            throw new RuntimeException("error al consultar el personas", e);
+        }
+    }
+
 }
